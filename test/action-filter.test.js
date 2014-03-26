26c587ed7547459a732a6db02a9537a4a92b489e
Nicolas Herment <nherment@gmail.com>
2014-03-26 09:46:34 +0000

    wy-filter now supports nested values
    fix #3

--- test/action-filter.test.js
+++ test/action-filter.test.js
@@ -13,9 +13,6 @@ describe('filter', function() {
     worky.reject(function() {
       assert.fail('unexpected reject')
     })
-    worky.modify(function() {
-      assert.fail('unexpected modify')
-    })
     worky.next(function() {
       done()
     })
@@ -29,9 +26,6 @@ describe('filter', function() {
 
     worky.reject(function() {
       assert.fail('unexpected reject')
-    })
-    worky.modify(function() {
-      assert.fail('unexpected modify')
     })
     worky.next(function() {
       done()
@@ -47,9 +41,6 @@ describe('filter', function() {
     worky.reject(function() {
       assert.fail('unexpected reject')
     })
-    worky.modify(function() {
-      assert.fail('unexpected modify')
-    })
     worky.next(function() {
       done()
     })
@@ -64,14 +55,53 @@ describe('filter', function() {
     worky.reject(function() {
       done()
     })
-    worky.modify(function() {
-      assert.fail('unexpected modify')
-    })
     worky.next(function() {
       assert.fail('unexpected next')
     })
 
     worky.run({name: "entity1", status: "new"})
+
+  })
+
+  it('attribute name can represent a nested value (match)', function(done) {
+    var worky = new WorkyMock(filter, {attributes: {"info.type": "cheese"}})
+
+    worky.reject(function() {
+      assert.fail('unexpected reject')
+    })
+    worky.next(function() {
+      done()
+    })
+
+    worky.run({info: {type: "cheese"}})
+
+  })
+
+  it('attribute name can represent a nested value (mismatch)', function(done) {
+    var worky = new WorkyMock(filter, {attributes: {"info.type": "cheese"}})
+
+    worky.reject(function() {
+      done()
+    })
+    worky.next(function() {
+      assert.fail('unexpected reject')
+    })
+
+    worky.run({info: {type: "ham"}})
+
+  })
+
+  it('attribute name can represent a nested value (missing nested object)', function(done) {
+    var worky = new WorkyMock(filter, {attributes: {"info.type": "cheese"}})
+
+    worky.reject(function() {
+      done()
+    })
+    worky.next(function() {
+      assert.fail('unexpected reject')
+    })
+
+    worky.run({})
 
   })
 
--- test/mock/WorkyMock.js
+++ test/mock/WorkyMock.js
@@ -30,14 +30,4 @@ WorkyMock.prototype.next = function(fn) {
   }
 }
 
-WorkyMock.prototype.modify = function(fn) {
-  if(fn) {
-    this._modify = fn
-  } else if(this._modify) {
-    this._modify()
-  } else {
-    throw new Error('unexpected call to "modify()"')
-  }
-}
-
 module.exports = WorkyMock
